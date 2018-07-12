4b874275b2734c32348f3687154cbc03f33aba4b
Erdem STAR <erdem-yildiz@windowslive.com>
2018-07-12 20:50:19 +0300

    MP4 video ve muzik indirilebiliyor

--- Manage/views.py
+++ Manage/views.py
@@ -5,6 +5,7 @@ from django.http import HttpResponseRedirect , HttpResponse
 from django.template import loader
 PY = ""
 from pytube import YouTube
+import os
 
 def Redirect(request):
     return HttpResponseRedirect("index")
@@ -20,13 +21,37 @@ def Index(request):
 
 def Download(request):
     global PY
+    MusicPath = os.getcwd() + "/Music"
+
     if request.method == "GET":
         return render(request, "download.html", {"ad" : PY.title ,"id" : PY.video_id })
+
     if request.method == "POST":
         tmp = request.POST["type"]
-        print tmp
-        return render(request, "download.html", {"ad" : "asd" ,"id" : "asd"})
+
+        if tmp == "music":
+            music = PY.streams.get_by_itag(140)
+
+            if music is None:
+                music = PY.streams.filter(subtype='mp4').first()
+                music.download(MusicPath)
+                print music
+            else:
+                print music
+                music.download(MusicPath)
 
 
+            return render(request, "download.html", {"ad": PY.title, "id": PY.video_id})
+
+        if tmp == "video":
+            videos = PY.streams.get_by_itag(22)
+
+            if videos is None:
+                videos = PY.streams.filter(subtype='mp4').filter(progressive=True).first()
+                videos.download(MusicPath)
+            else:
+                videos.download(MusicPath)
+
+            return render(request, "download.html", {"ad": PY.title +"(Video başarıyla indirildi)", "id": PY.video_id})
 
 
